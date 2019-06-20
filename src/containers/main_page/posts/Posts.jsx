27e26b1a1c9dac8da6fc608a1df6219593da6433
Sp <spherastree@gmail.com>
2019-06-20 23:01:01 +0300

    recme > add: infinity scroll and fix rec_system

--- src/containers/main_page/posts/Posts.jsx
+++ src/containers/main_page/posts/Posts.jsx
@@ -8,14 +8,19 @@ import article_logo from "../../../assets/text_logo.png"
 import link_logo from "../../../assets/ling_logo.png"
 import comment_logo from "../../../assets/comment-logo.svg"
 
+const Infinite = require('react-infinite');
+
 class Posts extends Component {
     componentDidMount() {
         this.getPost();
     };
     state = {
         posts: [],
-        user_id: this.props.user_id
+        user_id: this.props.user_id,
+        elements: this.buildElements(0, 20),
+        isInfiniteLoading: false
     };
+
     getPost = () => {
         axios.get('http://localhost:3001/posts', {
             headers: {
@@ -28,6 +33,7 @@ class Posts extends Component {
                 this.setState({posts});
             })
     };
+
     rPosts() {
         let p = null;
         p = this.state.posts.map(post => {
@@ -41,9 +47,8 @@ class Posts extends Component {
             if (post.cat_info === 'link') {
                 cat = link_logo;
             }
-
             return <Post
-                key = {post.id}
+                key={post.id}
                 postID={post.id}
                 PostClick={this.PostClick}
                 title={post.Title}
@@ -59,10 +64,86 @@ class Posts extends Component {
         });
         return p
     }
+
+    buildElements(start,end) {
+        let i = 0;
+        let elements = [];
+        setTimeout(() => {
+
+            let p = this.state.posts.map(post => {
+                let cat = null;
+                if (post.cat_info === 'video') {
+                    cat = video_logo;
+                }
+                if (post.cat_info === 'article') {
+                    cat = article_logo;
+                }
+                if (post.cat_info === 'link') {
+                    cat = link_logo;
+                }
+
+                if (i === start || i < end) {
+                    elements.push(<Post
+                        key = {post.id}
+                        postID={post.id}
+                        PostClick={this.PostClick}
+                        title={post.Title}
+                        LikeClick={this.LikeClick}
+                        countComment={post.comments}
+                        count_like={post.like}
+                        category_logo={cat}
+                        content={post.text}
+                        comment_logo={comment_logo}
+                        category={post.id_catigories}
+                        username={post.creator}
+                    />);
+                    i = i +1;
+                }
+                return null
+            });
+        }, 2000);
+
+        // console.log('elem: ',elements);
+        return elements
+    }
+
+    //inf list
+
+    handleInfiniteLoad = function() {
+        var that = this;
+        this.setState({
+            isInfiniteLoading: true
+        });
+        setTimeout(function() {
+            var elemLength = that.state.elements.length,
+                newElements = that.buildElements(elemLength, elemLength + 500);
+            that.setState({
+                isInfiniteLoading: false,
+                elements: that.state.elements.concat(newElements)
+            });
+        }, 2500);
+    };
+    //
+    elementInfiniteLoad = function() {
+        return <div className="infinite-list-item">
+            Loading...
+        </div>;
+    };
+
     render() {
         return (
             <div className="Posts">
+                <Infinite
+                    className={'inf'}
+                    elementHeight={115}
+                    containerHeight={900}
+                    infiniteLoadingBeginBottomOffset={200}
+                    onInfiniteLoad={this.handleInfiniteLoad}
+                    loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                    isInfiniteLoading={this.state.isInfiniteLoading}>
                 {this.rPosts()}
+
+                </Infinite>
             </div>
         )
     }
--- src/containers/user_like/user_like_post.jsx
+++ src/containers/user_like/user_like_post.jsx
@@ -8,20 +8,18 @@ import link_logo from "../../assets/ling_logo.png"
 import comment_logo from "../../assets/comment-logo.svg"
 import PostID from "../../components/post/Post";
 
+const Infinite = require('react-infinite');
+
 class user_like_post extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [],
-            user_id: this.props.user_id,
-        };
-    }
-
     componentDidMount() {
         this.getPost();
     };
-
+    state = {
+        posts: [],
+        user_id: this.props.user_id,
+        elements: this.buildElements(0, 20),
+        isInfiniteLoading: false
+    };
 
     getPost = () => {
         axios.post('http://localhost:3001/user_like', {
@@ -46,10 +44,12 @@ class user_like_post extends Component {
             if (post.cat_info === 'link') {
                 cat = link_logo;
             }
-
             return <PostID
+                key={post.id}
                 postID={post.id}
+                PostClick={this.PostClick}
                 title={post.Title}
+                LikeClick={this.LikeClick}
                 countComment={post.comments}
                 count_like={post.like}
                 category_logo={cat}
@@ -62,13 +62,89 @@ class user_like_post extends Component {
         return p
     }
 
+    buildElements(start,end) {
+        let i = 0;
+        let elements = [];
+        setTimeout(() => {
+
+            let p = this.state.posts.map(post => {
+                let cat = null;
+                if (post.cat_info === 'video') {
+                    cat = video_logo;
+                }
+                if (post.cat_info === 'article') {
+                    cat = article_logo;
+                }
+                if (post.cat_info === 'link') {
+                    cat = link_logo;
+                }
+
+                if (i === start || i < end) {
+                    elements.push(<PostID
+                        key = {post.id}
+                        postID={post.id}
+                        PostClick={this.PostClick}
+                        title={post.Title}
+                        LikeClick={this.LikeClick}
+                        countComment={post.comments}
+                        count_like={post.like}
+                        category_logo={cat}
+                        content={post.text}
+                        comment_logo={comment_logo}
+                        category={post.id_catigories}
+                        username={post.creator}
+                    />);
+                    i = i +1;
+                }
+                return null
+            });
+        }, 2000);
+
+        // console.log('elem: ',elements);
+        return elements
+    }
+
+    //inf list
+
+    handleInfiniteLoad = function() {
+        var that = this;
+        this.setState({
+            isInfiniteLoading: true
+        });
+        setTimeout(function() {
+            var elemLength = that.state.elements.length,
+                newElements = that.buildElements(elemLength, elemLength + 500);
+            that.setState({
+                isInfiniteLoading: false,
+                elements: that.state.elements.concat(newElements)
+            });
+        }, 2500);
+    };
+    //
+    elementInfiniteLoad = function() {
+        return <div className="infinite-list-item">
+            Loading...
+        </div>;
+    };
+
     render() {
         return (
             <div className="Posts">
-                {this.rPosts()}
+                <Infinite
+                    className={'inf'}
+                    elementHeight={115}
+                    containerHeight={900}
+                    infiniteLoadingBeginBottomOffset={200}
+                    onInfiniteLoad={this.handleInfiniteLoad}
+                    loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                    isInfiniteLoading={this.state.isInfiniteLoading}>
+                    {this.rPosts()}
+
+                </Infinite>
             </div>
         )
     }
 }
+
 
 export default user_like_post;
